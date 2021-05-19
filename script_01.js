b5a73d4af525cd8e9371e121a343f21a9b30ad96
PerOxiD692 <jan-daniel.martin@gmx.de>
2021-05-19 10:08:50 +0200

    if - alternativlos

--- script_01.js
+++ script_01.js
@@ -1,1 +1,25 @@
 // Entscheidungsstrukturen | control structures
+
+// Deklaration
+let ageJohn, ageMark;
+let isJohnOlder, isJohnEqual;
+
+// Wertzuweisung
+ageJohn = 30;
+ageMark = 30;
+
+// Logische Aussagen / Tests
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+
+// Ausgabe
+//console.log("John ist älter als Mark: " + isJohnOlder);
+//console.log("John hat das gleiche Alter wie Mark: " + isJohnEqual);
+
+// ***** IF *****
+// alternativlos (TINA - There is no alternative)
+
+if (true)
+{
+    console.log("John ist älter: ");
+}
